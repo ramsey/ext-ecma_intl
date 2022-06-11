5e45fdfe6de5e258fc3ebb583bc1ed979ac9c0ee
Ben Ramsey <ben@benramsey.com>
2022-06-11 16:53:27 -0500

    chore: set up repository for testing

---
+++ tests/phpunit/TestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ecma\Test\Intl;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * Configures and returns a mock object
+     *
+     * @param class-string<T> $class
+     *
+     * @return T & MockInterface
+     *
+     * @template T
+     */
+    public function mockery(string $class, mixed ...$arguments)
+    {
+        /** @var T & MockInterface $mock */
+        $mock = Mockery::mock($class, ...$arguments);
+
+        return $mock;
+    }
+}
