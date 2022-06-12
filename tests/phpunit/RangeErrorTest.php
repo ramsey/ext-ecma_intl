2e51bc91946564990f02496e1db1bb646ebadb08
Ben Ramsey <ben@benramsey.com>
2022-06-12 11:15:31 -0500

    chore: add missing PHPUnit tests

---
+++ tests/phpunit/RangeErrorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ecma\Test\Intl;
+
+use Ecma\Intl\Exception;
+use Ecma\Intl\RangeError;
+use RuntimeException;
+use ValueError;
+
+class RangeErrorTest extends TestCase
+{
+    public function testRangeErrorIsAChildOfValueError(): void
+    {
+        $exception = new RangeError();
+
+        $this->assertInstanceOf(ValueError::class, $exception);
+    }
+
+    public function testRangeErrorIsAnEcmaIntlException(): void
+    {
+        $exception = new RangeError();
+
+        $this->assertInstanceOf(Exception::class, $exception);
+    }
+
+    public function testRangeErrorThrownAndCaught(): void
+    {
+        $previous = new RuntimeException('A runtime exception');
+        $exception = new RangeError('A range error message', 36, $previous);
+
+        $this->expectException(RangeError::class);
+        $this->expectExceptionMessage('A range error message');
+        $this->expectExceptionCode(36);
+
+        throw $exception;
+    }
+}
